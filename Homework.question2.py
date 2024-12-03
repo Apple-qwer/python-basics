3985b353122d8b542cf7e5ecd177d25a4ed9d9c9
Jacob <applepie9284@gmail.com>
2024-12-03 10:18:29 -0500

    Delete Homework.question2.py

--- Homework.question2.py
+++
@@ -1,8 +0,0 @@
-s = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaapiwwwocnnnaioapuapoiuaopiuaodaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
-
-
-# Write further codes using pre-defined variable 
-b = s.index('b')
-d = s.index('d')
-words = s[b:d+1]
-print (words)
