5b3326e064cdca5d8b1c00b8c514ad6fbe9e6e3a
Jacob <applepie9284@gmail.com>
2024-12-03 10:20:32 -0500

    Update Homework.question2.py

--- Homework.questions/Homework.question2.py
+++ Homework.questions/Homework.question2.py
@@ -1,8 +1,6 @@
 #Q2. Slice the the string from b to d. 
 s = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaapiwwwocnnnaioapuapoiuaopiuaodaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
 
-
-# Write further codes using pre-defined variable 
 b = s.index('b')
 d = s.index('d')
 words = s[b:d+1]
