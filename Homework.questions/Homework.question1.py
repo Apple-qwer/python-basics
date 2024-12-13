5256dab02c3025ba1b8aae2d108f4b8a4357f990
Jacob <applepie9284@gmail.com>
2024-12-12 21:01:04 -0500

    Delete Homework.questions directory

--- Homework.questions/Homework.question1.py
+++
@@ -1,9 +0,0 @@
-#Q3. Find the remainder after dividing (5 * 607 - 88 / 2 * 10) by 7.
-
-x = (5 * 607 - 88 / 2 * 10) % 7 
-print (x)
-
-
-
-
-
--- Homework.questions/Homework.question2.py
+++
@@ -1,7 +0,0 @@
-#Q2. Slice the the string from b to d. 
-s = 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaabaapiwwwocnnnaioapuapoiuaopiuaodaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
-
-b = s.index('b')
-d = s.index('d')
-words = s[b:d+1]
-print (words)
